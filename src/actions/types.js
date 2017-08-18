ad41bf29d7ca9e08f3565355def10e5c8236f66f
Toyrone <akinstoyosi@gmail.com>
2017-08-18 15:24:44 +0100

    reverted commit

--- src/actions/types.js
+++
@@ -1,6 +0,0 @@
-export const GET_WANTED_LIST = 'get_wanted_list';
-export const ADD_PERSON = 'add_person_to_wanted_list';
-export const UPDATE_PERSON = 'update_person';
-export const DELETE_PERSON = 'delete_person_from_wanted_list';
-export const NEW_TOAST = 'new_toast';
-export const CLEAR_TOAST = 'clear_toast';
